d36e1ca5de8db08b2a7e6f91ad3fea5c3f4c3c8b
jesmoo <habidmoo@gmail.com>
2022-01-27 17:52:39 -0600

    :white_check_mark: progress
    agregando el componente progress con su test unitario

---
+++ src/components/Progress.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style/components/Progress.css';
+
+const Progress = () => {
+  return (
+    <div className="main__progress">
+      <span className="progress__icon gray" />
+      <span className="progress__icon gray" />
+      <span className="progress__icon gray" />
+      <span className="progress__icon gray" />
+    </div>
+  );
+};
+
+export default Progress;
---
+++ src/container/MainHome.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Progress from '../Components/Progress';
+import '../style/containers/MainHome.css';
+
+const MainHome = () => {
+  return (
+    <>
+      <Progress />
+    </>
+  );
+};
+
+export default MainHome;
