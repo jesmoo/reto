77e27cf5a3c9c1c67c047912d6489677a20684fd
jesmoo <habidmoo@gmail.com>
2022-01-27 18:38:59 -0600

    :lipstick: password conditions
    agregando el componente de las condiciones de la contraseña

---
+++ src/components/PasswordText.jsx
@@ -0,0 +1,95 @@
+import React, { useRef, useEffect, useState } from 'react';
+import Input from './Input';
+import Btns from './Btns';
+
+import '../style/components/PasswordText.css';
+
+const PasswordText = ({ passConditional, email }) => {
+  const [passwordCheck, setPasswordCheck] = useState({});
+  const [samePassword, setSamePassword] = useState(0);
+
+  const charactesPassword = '!”#$%&/()=?¿^*@‚[]{};:_><,.-|`+.';
+
+  const characters = useRef();
+  const number = useRef();
+  const characterSpecial = useRef();
+  const sentence = useRef();
+  const repetiveString = useRef();
+  const repetiveNumber = useRef();
+
+  const validationArray = [
+    characters,
+    number,
+    characterSpecial,
+    sentence,
+    repetiveString,
+    repetiveNumber,
+  ];
+
+  let pasword = passConditional;
+
+  const handlePasswordCheck = (e) => {
+    e.preventDefault();
+
+    const PasswordValue = e.target.value;
+    setPasswordCheck({ ...passwordCheck, typed: PasswordValue });
+    if (pasword === PasswordValue) {
+      setSamePassword(samePassword + 1);
+    } else {
+      setSamePassword(0);
+    }
+  };
+
+  return (
+    <section className="main__passwordCheck">
+      <p className="password-Alert">
+        Por razones de seguridad tu contraseña debe tener las siguientes
+        carateristicas:
+      </p>
+      <div className="password-rule">
+        <span ref={characters} className="rule-icon red" />
+        <p className="rule-text">
+          Mínimo 6 caracteres (letras, números y caracteres especiales).
+        </p>
+      </div>
+      <div className="password-rule">
+        <span ref={number} className="rule-icon red" />
+        <p className="rule-text">Mínimo 1 número.</p>
+      </div>
+      <div className="password-rule">
+        <span ref={characterSpecial} className="rule-icon red" />
+        <p className="rule-text">{`Mínimo 1 de estos caracteres especiales ${charactesPassword}`}</p>
+      </div>
+      <div className="password-rule">
+        <span ref={sentence} className="rule-icon red" />
+        <p className="rule-text">No tener la frase “100Ladrillos”.</p>
+      </div>
+      <div className="password-rule">
+        <span ref={repetiveString} className="rule-icon red" />
+        <p className="rule-text">
+          No tener mas de 3 caracteres idénticos en forma consecutiva (ej: aaa).
+        </p>
+      </div>
+      <div className="password-rule">
+        <span ref={repetiveNumber} className="rule-icon red" />
+        <p className="rule-text">
+          No tener mas de 3 caracteres numéricos y/o letras en forma secuencial
+          (ej: 123).
+        </p>
+      </div>
+      <Input
+        classSection={'passwordCheck__again'}
+        text={'Confirma tu contraseña'}
+        type={'password'}
+        place={'Contraseña'}
+        handle={handlePasswordCheck}
+        value={passwordCheck.typed || ''}
+      />
+      {samePassword ? (
+        <Btns classNames={'passwordCheck-btn'} text={'Continuar'} />
+      ) : null}
+    </section>
+  );
+};
+
+export default PasswordText;
